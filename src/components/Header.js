6e7b7756edc78af55766fb789338c6ffbaf5046c
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2023-05-17 02:47:46 +0700

    Finish header design

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+function Header() {
+	return (
+		<div className="header">
+			<h1>Store</h1>
+			<div className="links">
+				<ul>
+					<li>Home</li>
+					<li>Product</li>
+					<li>Contact</li>
+					<li>
+						<button>fdsfdsf</button>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
